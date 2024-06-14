b548d555d2fc3713b2c970c1649fd8020e174507
Saikat123Dev <berasaikat729@gmail.com>
2024-06-14 17:06:11 +0530

    update-register-login

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -1,14 +1,9 @@
-"use client";
-
-import { 
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { Header } from "@/components/auth/header";
 import { Social } from "@/components/auth/social";
 import { BackButton } from "@/components/auth/back-button";
+import Image from 'next/image';
+import authimg from "@/images/wallpaperflare.com_wallpaper.jpg";
 
 interface CardWrapperProps {
   children: React.ReactNode;
@@ -16,9 +11,9 @@ interface CardWrapperProps {
   backButtonLabel: string;
   backButtonHref: string;
   showSocial?: boolean;
-};
+}
 
-export const CardWrapper = ({
+ const CardWrapper = ({
   children,
   headerLabel,
   backButtonLabel,
@@ -26,24 +21,44 @@ export const CardWrapper = ({
   showSocial
 }: CardWrapperProps) => {
   return (
-    <Card className="w-[400px] shadow-md">
-      <CardHeader>
-        <Header label={headerLabel} />
-      </CardHeader>
-      <CardContent>
-        {children}
-      </CardContent>
-      {showSocial && (
-        <CardFooter>
-          <Social />
-        </CardFooter>
-      )}
-      <CardFooter>
-        <BackButton
-          label={backButtonLabel}
-          href={backButtonHref}
-        />
-      </CardFooter>
-    </Card>
+    <div className="flex justify-center items-center w-full h-screen bg-green-200">
+      <Card className="w-[80%] h-[80%] flex flex-col justify-center items-center p-8">
+        <div className="flex flex-col md:flex-row w-full h-full">
+          {/* Left half for the image */}
+          <div className="w-full md:w-1/2 h-[400px] md:h-auto relative">
+            <Image
+              src={authimg}
+              alt="Auth Image"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg"
+            />
+          </div>
+          {/* Right half for the card */}
+          <div className="w-full md:w-1/2 px-4 py-8">
+            <Card className="shadow-lg border-black w-full h-full flex flex-col justify-center">
+              <CardHeader>
+                <Header label={headerLabel} />
+              </CardHeader>
+              <CardContent>
+                {children}
+              </CardContent>
+              {showSocial && (
+                <CardFooter>
+                  <Social />
+                </CardFooter>
+              )}
+              <CardFooter>
+                <BackButton
+                  label={backButtonLabel}
+                  href={backButtonHref}
+                />
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 };
+export default CardWrapper
